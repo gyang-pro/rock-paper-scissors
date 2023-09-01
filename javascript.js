b2312c154127e4076cc88126b30871133b3e8ca9
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-01 12:55:13 -0700

    Create pseudocode for getComputerChoice function and to play a single game of 'Rock, Paper, Scissors'

---
+++ javascript.js
@@ -0,0 +1,26 @@
+//randomly returns rock, paper, or scissors
+function getComputerChoice() {
+    //create a variable called randomNumber
+    //initialize it using a random number generator
+    //if randomNumber <= 33, return rock
+    //else if randomNumber <=66, return paper
+    //else return scissors
+}
+
+//create a function that takes two parameters 'playerSelection' and 'computerSelection'
+//create variable called winner
+//turn playerSelection to all lower-case
+//if playerSelection and computerSelection are same, winner = "Tie"
+//else if playerSelction = rock:
+    //if computerSelection = scissors, winner = playerSelection
+    //if computerSelection = paper, winner = computerSelection
+//else if playerSelection = scissors:
+    //if computerSelection = rock, winner = computerSelection
+    //if computerSelection = paper, winner = playerSelection
+//else if playerSelection = paper:
+    //if computerSelection = rock, winner = playerSelection
+    //if computerSelection = scissors, winner = computerSelection
+
+//if winner = "Tie", output "playerSelection and computerSelection ties!"
+//else if winner = playerSelection, output "You win! playerSelection beats computerSelection"
+//else if winner = computerSelection, output "You lose! computerSelection beats playerSelection"
