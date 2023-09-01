b57a62a2f82eaae5475014eb0675c2db40e51e28
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-01 14:00:53 -0700

    Remove pseudocode comments

--- javascript.js
+++ javascript.js
@@ -1,18 +1,13 @@
 //randomly returns rock, paper, or scissors
 function getComputerChoice() {
-    //create a variable called randomNumber
-    //initialize it using a random number generator
+    //generate a random number between 0 and 100
     let randomNumber = Math.floor(Math.random() * 100) + 1;
-    //if randomNumber <= 33, return rock
+
     if(randomNumber <= 33) {
         return 'rock';
-    }
-    //else if randomNumber <=66, return paper
-    else if(randomNumber <= 66) {
+    } else if(randomNumber <= 66) {
         return 'paper';
-    }
-    //else return scissors
-    else {
+    } else {
         return 'scissors'
     }
 
@@ -20,61 +15,38 @@ function getComputerChoice() {
 
 //create a function that takes two parameters 'playerSelection' and 'computerSelection'
 function playRound(playerSelection, computerSelection) {
-    //create variable called winner
     let winner;
-    //turn playerSelection to all lower-case
     playerSelection = playerSelection.toLowerCase();
-    //if playerSelection and computerSelection are same, winner = "Tie"
+
     if(playerSelection === computerSelection) {
         winner = "Tie";
-    }
-    //else if playerSelction = rock:
-    else if(playerSelection === 'rock') {
-        //if computerSelection = scissors, winner = playerSelection
+    } else if(playerSelection === 'rock') {
         if(computerSelection === 'scissors') {
             winner = playerSelection;
-        }
-        //if computerSelection = paper, winner = computerSelection
-        else {
+        } else {
             winner = computerSelection;
         }
-    }
-    //else if playerSelection = scissors:
-    else if(playerSelection === 'scissors') {
-        //if computerSelection = rock, winner = computerSelection
+    } else if(playerSelection === 'scissors') {
         if(computerSelection === 'rock') {
             winner = computerSelection;
-        }
-        //if computerSelection = paper, winner = playerSelection
-        else {
+        } else {
             winner = playerSelection;
         }
-    }
-    //else if playerSelection = paper:
-    else if(playerSelection === 'paper') {
-        //if computerSelection = rock, winner = playerSelection
+    } else if(playerSelection === 'paper') {
         if(computerSelection === 'rock') {
             winner = playerSelection;
-        }
-        //if computerSelection = scissors, winner = computerSelection
-        else {
+        } else {
             winner = computerSelection;
         }
-    }
-    else {
+    } else {
         return `Invalid input. Please pick 'rock', 'paper', or 'scissors' to play this game.`;
     }
 
-    //if winner = "Tie", output "playerSelection and computerSelection ties!"
     if(winner === "Tie") {
         return `${playerSelection} and ${computerSelection} ties!`;
-    }
-    //else if winner = playerSelection, output "You win! playerSelection beats computerSelection"
-    else if(winner === playerSelection) {
+    } else if(winner === playerSelection) {
         return `You win! ${playerSelection} beats ${computerSelection}`;
-    }
-    //else if winner = computerSelection, output "You lose! computerSelection beats playerSelection"
-    else if(winner === computerSelection) {
+    } else if(winner === computerSelection) {
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
